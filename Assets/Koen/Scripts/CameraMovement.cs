53e47f7409230ecdfa7aae535cbd87dbb521c40e
KoenBos <koenbos@outlook.com>
2023-03-18 14:38:33 +0100

    Update Camara

--- Assets/Koen/Scripts/CameraMovement.cs
+++ Assets/Koen/Scripts/CameraMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+
+    bool anySelected = false;
     void Update()
     {
         //is there is a character selected the camera will not move and will follow the character and zoom in and out with the character
@@ -11,10 +13,15 @@ public class CameraMovement : MonoBehaviour
         {
             if (character.GetComponent<AI>().selected == true)
             {
+                anySelected = true;
                 //the camera will follow the character with a lerping effect
                 transform.position = Vector3.Lerp(transform.position, new Vector3(character.transform.position.x, character.transform.position.y, -10), Time.deltaTime * 2);
                 //the camera will zoom in and out with the character with a lerping effect without a maxspeed
                 GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, 3f, Time.deltaTime * 1);
+            }
+            else
+            {
+                anySelected = false;
             }
         }
         //if the mouse is pressed down and moved the camera will move with the mouse movement based on the orthographic size of the camera
@@ -24,11 +31,11 @@ public class CameraMovement : MonoBehaviour
             transform.Translate(-Input.GetAxis("Mouse X") * Time.deltaTime * 10, -Input.GetAxis("Mouse Y") * Time.deltaTime * 10, 0);
         }
         //scrolling in and out will change the orthographic size of the camera to zoom in and out
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        if (anySelected  == false && Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             GetComponent<Camera>().orthographicSize -= 1;
         }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        if (anySelected  == false && Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             GetComponent<Camera>().orthographicSize += 1;
         }
