e36cb397b2c923967e10cf2ce58caa755605021c
KoenBos <koenbos@outlook.com>
2023-03-18 15:13:49 +0100

    Fixes

--- Assets/Koen/Scripts/Spawner.cs
+++ Assets/Koen/Scripts/Spawner.cs
@@ -17,7 +17,10 @@ public class Spawner : MonoBehaviour
             for (int i = 0; i < 10; i++)
             {
                 //create a new character from the prefab
-                GameObject character = Instantiate(prefab, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0), Quaternion.identity);
+                GameObject character = Instantiate(prefab, new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity, transform);
+
+                //add the character to layer 3 (characters)
+                character.layer = 3;
                 
                 //put the character in the list
                 characters.Add(character);
