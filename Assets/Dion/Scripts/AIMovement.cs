712673d97219a813ae0528387e17ac4ca1ce4dd5
DionKoenes <koenesdion@gmail.com>
2023-04-20 15:27:32 +0200

    DionScene Updated

--- Assets/Dion/Scripts/AIMovement.cs
+++ Assets/Dion/Scripts/AIMovement.cs
@@ -12,19 +12,21 @@ public class AIMovement : MonoBehaviour
     void Start()
     {
         // Set the initial direction of the character to a random direction
-        direction = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+        transform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5), 0f);
     }
+
 
     // Update is called once per frame
     void Update()
     {
         // Move the character in the current direction
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position += new Vector3(direction.x, direction.y, 0f) * speed * Time.deltaTime;
 
         // If the character has moved too far in the current direction, choose a new direction
         if (transform.position.magnitude > 10f)
         {
-            direction = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+            direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
         }
     }
 }
